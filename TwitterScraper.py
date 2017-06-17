882f25bf69bb6c798c20bfd385b0a2f74f0a868b
elias <elias.jm.garcia@gmail.com>
2017-06-17 18:04:47 -0500

    got working search query for distance + location

--- TwitterScraper.py
+++ TwitterScraper.py
@@ -241,7 +241,7 @@ class TwitterSlicer(TwitterSearch):
             if tweet['created_at'] is not None:
                 t = datetime.datetime.fromtimestamp((tweet['created_at']/1000))
                 fmt = "%Y-%m-%d %H:%M:%S"
-                log.info("%i [%s] - %s" % (self.counter, t.strftime(fmt), tweet['text']))
+                log.info("%i [%s] - %s/status/%s" % (self.counter, t.strftime(fmt), tweet['user_name'], tweet['tweet_id']))
 
         return True
 
@@ -249,22 +249,23 @@ class TwitterSlicer(TwitterSearch):
 if __name__ == '__main__':
     log.basicConfig(level=log.INFO)
 
-    search_query = 'babylon 5 near: '
+    # format MUST BE <any search words/query>[space]near:["][location]["][space]within:[distance]mi
+    search_query = 'allergies OR sneezing near:"Kansas City, MO" within:1700mi'
     rate_delay_seconds = 0
     error_delay_seconds = 5
 
     # Example of using TwitterSearch
-    twit = TwitterSearchImpl(rate_delay_seconds, error_delay_seconds, None)
-    twit.search(search_query)
+    # twit = TwitterSearchImpl(rate_delay_seconds, error_delay_seconds, None)
+    # twit.search(search_query)
 
     # Example of using TwitterSlice
     select_tweets_since = datetime.datetime.strptime("2016-10-01", '%Y-%m-%d')
-    select_tweets_until = datetime.datetime.strptime("2016-12-01", '%Y-%m-%d')
+    select_tweets_until = datetime.datetime.strptime("2017-10-02", '%Y-%m-%d')
     threads = 10
 
     twitSlice = TwitterSlicer(rate_delay_seconds, error_delay_seconds, select_tweets_since, select_tweets_until,
                               threads)
     twitSlice.search(search_query)
 
-    print("TwitterSearch collected %i" % twit.counter)
+    # print("TwitterSearch collected %i" % twit.counter)
     print("TwitterSlicer collected %i" % twitSlice.counter)
